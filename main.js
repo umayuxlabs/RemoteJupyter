cb33da4e91969d66048820d78ea79ed30de26bb4
Arango Argoty, Gustavo Alonso <gustavo.arango@astrazeneca.com>
2021-11-08 17:56:52 -0500

    added session manager

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 
+app.commandLine.appendSwitch ("disable-http-cache");
+
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
@@ -17,15 +19,39 @@ function createWindow () {
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
+  mainWindow.webContents.on('did-create-window', (childWindow) => {
+    // For example...
+    childWindow.webContents.on('will-navigate', (e) => {
+      e.preventDefault()
+    })
+  })
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
 
-const nativeMenus = [
+const isMac = process.platform === 'darwin'
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
   {
     label: 'File',
     submenu: [
+      isMac ? { role: 'close' } : { role: 'quit' }, 
       {
         label: 'New Window',
         click() {
@@ -33,10 +59,82 @@ const nativeMenus = [
         }
       }
     ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { role: 'toggleDevTools' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://electronjs.org')
+        }
+      }
+    ]
   }
 ]
 
-const menu = Menu.buildFromTemplate(nativeMenus)
+const menu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(menu)
 
 // This method will be called when Electron has finished
--- renderer.js
+++ renderer.js
@@ -8,16 +8,50 @@
 const button = document.querySelector('#connect');
 const nodename = document.querySelector('#nodename');
 const url = document.querySelector('#url');
-const panel = document.querySelector('#panel')
+
+var session_list = new Vue({
+    el: '#session-list',
+    data: {
+      items: JSON.parse(localStorage.getItem('session_list')) == undefined ? []: JSON.parse(localStorage.getItem('session_list'))
+    },
+
+    methods: {
+        run: function(item){
+            windowProxy = window.open('session.html?JupyterURL='+item.url + "&JupyterTitle="+item.session, item.session, 'minimizable=true')
+        },
+        remove: function(item){
+
+            session_list.items = session_list.items.filter((i)=>{
+                if (i == item){
+                    delete session_dict[i.session + i.url]
+                }
+                
+                return i != item
+            })
+
+            localStorage.setItem('session_list', JSON.stringify(session_list.items))
+            localStorage.setItem('session_dict', JSON.stringify(session_dict))
+        }
+    }
+
+  })
 
 button.addEventListener('click', updateButton);
+var windowProxy = null;
 
+let session_dict = JSON.parse(localStorage.getItem('session_dict')) == undefined ? {}: JSON.parse(localStorage.getItem('session_dict')) 
+let session_items = []
 function updateButton() {
-//   window.location.assign(url.value)
+    windowProxy = window.open('session.html?JupyterURL='+url.value + "&JupyterTitle="+nodename.value, nodename.value, 'minimizable=true')
+    if(session_dict[nodename.value + url.value] == undefined){
+        session_list.items.push({session: nodename.value, url: url.value})
+        session_dict[nodename.value + url.value] = 0
 
-    const webview = document.querySelector('webview')
-    webview.src = url.value
-    webview.style.top = '0px'
-    
-    document.title = 'Session: ' + nodename.value
+        localStorage.setItem('session_dict', JSON.stringify(session_dict))
+        localStorage.setItem('session_list', JSON.stringify(session_list.items))
+    }
+}
+
+function run_job(session, url){
+    console.log(session, url)
 }
